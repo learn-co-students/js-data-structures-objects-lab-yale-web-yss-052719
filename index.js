71ba56fc981ade9c7da5e8e138e1b51116f56c59
jqzheng617 <jenna.zheng617@gmail.com>
2019-07-27 15:11:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+// define driver
+let driver = {};
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  return Object.assign({}, driver, { [key]: value });
+  // need to merge into empty {} for it to be nondestructive
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  return Object.assign(driver, { [key]: value });
+}
+
+function deleteFromDriverByKey(driver, key) {
+  let clone = Object.assign({}, driver)
+  delete clone[key];
+  return clone;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  let clone = Object.assign(driver)
+  delete clone[key];
+  return clone; 
+}
